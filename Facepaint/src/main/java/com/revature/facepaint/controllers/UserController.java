c12b01f6acd2f468443f21c4f3b329ae6101ae39
Robert (Trey) Ratcliff <robert089@revature.net>
2022-07-21 10:31:29 -0400

    Added login feature. role_id does not properly map to the user_role table which will cause Enum issues, assuming this is from the create tag in the application.yml. Otherwise, login works as intended.

--- Facepaint/src/main/java/com/revature/facepaint/controllers/UserController.java
+++ Facepaint/src/main/java/com/revature/facepaint/controllers/UserController.java
@@ -1,14 +1,26 @@
 package com.revature.facepaint.controllers;
 
+import com.revature.facepaint.dto.UserDTO;
+import com.revature.facepaint.exceptions.UserNotFoundException;
+import com.revature.facepaint.model.User;
+import com.revature.facepaint.services.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 
 import com.revature.facepaint.services.UserService;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+//@Controller
+
+//Use this to specify all methods should return a response body.
+@RestController
+@RequestMapping("/users")
 public class UserController {
 	
 	private UserService us;
+	private AuthService as;
 
 	public UserController() {
 		super();
@@ -16,9 +28,25 @@ public class UserController {
 	}
 	
 	@Autowired
-	public UserController(UserService us) {
+	public UserController(UserService us, AuthService as) {
 		super();
 		this.us = us;
+		this.as = as;
 	}
 
+
+	@GetMapping
+	public ResponseEntity<UserDTO> getByLogin(@RequestParam(name="username") String username, @RequestParam(name="password") String password){
+
+		try{
+			User user = as.getByLogin(username, password);
+			System.out.println(user);
+			UserDTO userDTO = new UserDTO(user);
+			return new ResponseEntity<>(userDTO, HttpStatus.OK);
+
+		}catch (UserNotFoundException | NullPointerException e){
+			e.getStackTrace();
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+	}
 }
