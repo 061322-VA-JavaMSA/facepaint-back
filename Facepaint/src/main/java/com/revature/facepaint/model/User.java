6b839b24bd0f68d18fbd214acf29979d0b9bfbde
Robert (Trey) Ratcliff <robert089@revature.net>
2022-07-25 17:56:13 -0400

    added Id tag

--- Facepaint/src/main/java/com/revature/facepaint/model/User.java
+++ Facepaint/src/main/java/com/revature/facepaint/model/User.java
@@ -14,6 +14,7 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "users")
 public class User {
+	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	@Column(unique = true)
